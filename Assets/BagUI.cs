cbb8afa094b35fdf077e3b56c3e8a6939184e289
LT0143 <1162710759@qq.com>
2017-05-25 10:21:08 +0800

    test

---
+++ Assets/BagUI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FairyGUI;
+using FairyGUI.Utils;
+
+public  class BagUI :GComponent
+   {
+       public const string url = "ui://vmts0v9ewa2di"; 
+    private GList list;
+    private GComponent cell;
+    public override void ConstructFromXML(XML xml)
+       {
+           base.ConstructFromXML(xml);
+        list = GetChild("list_bag").asList;
+        BuildCell();
+    }
+
+    public void NewCell()
+    {
+        cell = UIPackage.CreateObject("test", "Button10").asCom;
+        list.AddChild(cell);
+        cell.draggable = true;
+       cell.onDragStart.Add((EventContext ctx) =>
+       {
+           DragDropManager.inst.Cancel();
+           ctx.PreventDefault();  //阻止格子响应，防止格子被拖拽
+           //DragDropManager.inst.StartDrag(cell,cell.icon);
+       });
+
+        cell.onDrop.Add((EventContext cxt) =>
+        {
+            if (cxt.sender is GButton )
+            {
+
+            }
+        });
+    }
+
+    public void BuildCell()
+    {
+        list.RemoveChildren();
+        Bag bag = Bag.Instance;
+        for (int i = 0; i < bag.CellList.Count; i++)
+        {
+            NewCell();
+        }
+
+    }
+}
+ 
